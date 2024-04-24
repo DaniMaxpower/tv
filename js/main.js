f8265cd5de4d0563e8254b2eb419d8b75145aa63
Danimaxpower <maxpowerrubio@gmail.com>
2024-04-24 13:52:40 +0200

    poniendo variables con let y const y vinculando con el DOM

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const canales = [
+    { nombre : "canal 1", background: ""},
+    { nombre : "canal 2", background: "./"},
+    { nombre : "canal 3", background: "./"},
+    { nombre : "canal 4", background: "./"},
+    { nombre : "canal 5", background: "./"},
+    
+    
+];
